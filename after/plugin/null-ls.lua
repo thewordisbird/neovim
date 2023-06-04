78aeaeb91e7bca9d8a89d36f8bbe852556136e27
Justin Bird <justin.bird@dubsado.com>
2023-06-03 21:38:20 -0700

    null-ls format on save

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,10 +1,30 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        --null_ls.builtins.diagnostics.eslint,
-        --null_ls.builtins.completion.spell,
-    },
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettier,
+		--null_ls.builtins.diagnostics.eslint,
+		--null_ls.builtins.completion.spell,
+	},
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.format({
+						bufnr = bufnr,
+						filter = function(client)
+							return client.name == "null-ls"
+						end,
+					})
+					-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+				end,
+			})
+		end
+	end,
 })
